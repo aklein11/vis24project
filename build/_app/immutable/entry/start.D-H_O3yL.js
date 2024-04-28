458cbe9794e2ccc3bf5089caa84201b6182de061
Caroline Cunningham <ccunning@mit.edu>
2024-04-28 18:39:07 -0400

    all on 1 page

---
+++ build/_app/immutable/entry/start.D-H_O3yL.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.B2z6EwsB.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,9 @@
         currentPage %= 4;
     }
 
+    const cambridgeZip = ["02140", "02139", "02142", "02141", "02138"];
+
+
 </script>
 
 <svelte:head>
@@ -29,11 +32,21 @@
 
 <!-- Separate sections with unique class for each type of chart -->
 <style>
-    .scrolly-container {
-        margin-bottom: 2rem; /* Add space between each Scrolly component */
+   .scrolly-container {
+        border: 2px solid #rgb(8, 8, 8);
+        max-width: 100%;
     }
-	:global(body) {
-        max-width: min(120ch, 80vw);
+
+    .pie-container {
+        border: 2px solid #rgb(8, 8, 8);
+        max-width: 100%;
+    }
+
+    /* .scrolly {
+        max-width: 50%;
+    } */
+    .parent-container {
+    margin: auto; /* Center the container horizontally */
     }
 
    .stats {
@@ -54,17 +67,36 @@
         grid-row: 2;  /* Place dd in the second row */
     }
 
-	/* CSS file */
-	.graph-container {
-		width: 100px; /* Set the desired width */
-		border: 1px solid #ccc; /* Optional: Add a border for visualization */
-	}
-
 	.graph-container svg {
 		width: 100%; /* Set the width of the graph to 100% of its container */
 		height: 100%; /* Set the height of the graph to 100% of its container */
 	}
+
+    .hide-graph {
+        visibility: none;
+    }
+
+    .test {
+        font-size: 20px;
+        margin-top: 50px;
+        font-weight: normal;
+        margin-left: 0px;
+        overflow: scroll;
+    }
+
+    .test-pie {
+        font-size: 20px;
+        margin-top: 50px;
+        font-weight: normal;
+        margin-left: 0px;
+        overflow: scroll;
+    }
+
+
+
 </style>
+
+<h1> Condo Conversions in Greater Boston </h1>
 
 <blockquote scrolly-container>
 <p> <a href="https://www.mapc.org/wp-content/uploads/2021/12/10.-MC2050-Homes-for-Everyone-Brief.pdf">Greater Boston ranks 3rd in the most expensive housing markets in the United States, </a> and <a href="https://homesforprofit.mapc.org/report"> 51% of Boston residents are forced to spend over the recommended income percentage (30%) on rent. </a>
@@ -129,6 +161,84 @@ We take you through three examples of condo conversion in Greater Boston.
 {/if}
 
 <text> <a href="https://svelte.dev/examples/context-api"> Image Generation Credit </a> </text>
+
+<h1> Visualize Impact in Your Zipcode </h1>
+
+
+<blockquote>
+    Through our interactive visualization, we invite you to explore the impact of condo conversions on your community.<br>
+    <newline></newline>
+    On the map, click a Boston or Cambridge zip code to see the locations of condo conversions (starting from 2015) in the region:
+</blockquote>
+
+<div class="parent-container">
     
+    <Map bind:zipcode={ $zipcode }/>
+
+    <Scrolly>
+        <p class="test"> Section 1: How Old are Condos that are Most Likely to be Converted?
+        </p>
+        <svelte:fragment slot="viz">
+            <div class="scrolly-container">
+                <Bargraph query={ $zipcode }/>
+            </div>
+        </svelte:fragment>
+    </Scrolly>
+    
+    <Scrolly>
+        <p class="test-pie">Section 2: What Types of Buildings are Most Likely to be Converted? </p>
+        <svelte:fragment slot="viz">
+            <div class="pie-container">
+                <Piechart query={ $zipcode }/> 
+            </div>
+        </svelte:fragment>
+    </Scrolly>
+</div>
+
+{#if $zipcode !== '' && cambridgeZip.indexOf($zipcode) !== -1}
+    <h1> Condo Complaints </h1>
+    <blockquote>
+        For Cambridge, we did not have data on condominium complaints.
+    </blockquote>
+{:else}
+    <Scrolly>
+        <p class="test">Section 3: How do Complaints about Condominiums vary over Time?</p>
+        <svelte:fragment slot="viz">
+            <div class="scrolly-container">
+                <Linechart query={ $zipcode }/>
+            </div>
+        </svelte:fragment>
+    </Scrolly>
+
+    <Scrolly>
+        <p class="test">Section 4: What Types of Complaints are Most Common?</p>
+        <svelte:fragment slot="viz">
+            <div class="scrolly-container">
+                <Bubblechart query={ $zipcode }/>
+            </div>
+        </svelte:fragment>
+    </Scrolly>
+{/if}
+
+<h1> What is Being Done? </h1>
+
+<blockquote scrolly-container>
+    <p>Boston has passed protections on condo conversions of older buildings; specifically, the <a href="https://www.boston.gov/condominium-and-cooperative-conversion-ordinance"> Condominium and Cooperative Conversion Ordinance (c.a. 1999, amended 2021) </a> provides rights to tenants of buildings constructed pre-1983 with 4+ rental units. 
+        <a href="https://www.boston.gov/sites/default/files/file/2022/08/Conversion%20Ordinance%20Summary%2C%20English.pdf"> Specifically, the ordinance specifies a required notification period, a set relocation payment, and the requirement that tenants have the right to purchase their unit, among other protections. </a>
+    </p>
+
+    <p>
+    Legal ordinances communities in greater Boston have to protect tenants are detailed on Mass Legal Help's website <a href="https://www.masslegalhelp.org/housing-apartments-shelter/rooming-houses-mobile-homes-condos/local-protections-tenants-facing"> here </a>.
+    </p>
+
+    <p>
+    That being said, protections for tenants facing condo conversions is still lacking. The Metropolitan Area Planning Council (MAPC) writes in their <a href="https://homesforprofit.mapc.org/report"> report </a> on investor activity in Greater Boston from 2000-2022 that Investor purchases account for 21% of transactions, contributing to housing unaffordability and price increases. For more information on proposed policies, including proposed bills to help residents find short-term emergency housing and increase funds for affordable housing, view the Policy Recommendations section of the <a href="https://homesforprofit.mapc.org/report"> MAPC report </a>.
+    </p>    
+
+    <p>    
+    Thank you to the Metropolitan Area Planning Council for guidance on our project!
+    </p>
+    </blockquote>
+
 
 
